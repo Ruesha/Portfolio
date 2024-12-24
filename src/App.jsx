90f5b3728a0eb8cf3a0a9dca22335a9b9b07cc2b
ruesha <princeadewale2005@gmail.com>
2024-12-24 11:38:05 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,33 @@
-import Homepage from "./Pages/Homepage"
-import Techstack from "./Pages/Techstack"
-import Header from "./Static/Header/Header"
-import { BrowserRouter, Route, Routes} from 'react-router-dom'
-import MyProjects from './Pages/MyProjects'
-
-import Footer from "./Static/Footer/Footer"
-import Contact from "./Pages/Contact"
-import Aboutme from "./Components/Aboutme/Aboutme"
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Homepage from './Pages/Homepage';
+import Techstack from './Pages/Techstack';
+import MyProjects from './Pages/MyProjects';
+import Contact from './Pages/Contact';
+import Aboutme from './Components/Aboutme/Aboutme';
+import Header from './Static/Header/Header';
+import Footer from './Static/Footer/Footer';
 
 function App() {
-
   return (
     <div className="body">
-      <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/techstack" element={<Techstack />} />
-       <Route path="/pro" element={<MyProjects/>} />
-       <Route path="/contact" element={<Contact />} />
-       <Route path="/about" element={<Aboutme/>} />
-      </Routes>
-      <Footer />
-      </BrowserRouter> 
+   
+      <BrowserRouter basename="/">
+       
+        <Header />
+
+ 
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/techstack" element={<Techstack />} />
+          <Route path="/pro" element={<MyProjects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<Aboutme />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
